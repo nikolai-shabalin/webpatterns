99783e1fd07d6ea75a5171e4d45737a304c75a76
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2025-08-12 22:43:45 +0300

    Дописывает статью

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      // Выбираем тему для подсветки синтаксиса
+      theme: 'github-dark',
+      // Включаем подсветку для всех языков
+      wrap: true
+    }
+  }
+});
